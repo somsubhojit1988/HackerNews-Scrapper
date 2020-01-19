7c3494dd9fcd7ce3fc5b8e910f1104a5e1ec24ac
Subhojit Som <subhojit.som@macys.com>
2020-01-19 10:22:41 -0500

    removing commented code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -26,39 +25,6 @@ func fetchHnPage(url string) (*http.Response, error) {
 	return client.Do(req)
 }
 
-func saveResponse(res *http.Response, logger *log.Logger) {
-
-	file, err := os.Create("html-response.html")
-	if err != nil {
-		logger.Printf("error creating file: %v\n", err.Error())
-		return
-	}
-
-	defer file.Close()
-	defer res.Body.Close()
-
-	bufferedWrite := func(res *http.Response, file *os.File, logger *log.Logger) int64 {
-		buff := make([]byte, 128)
-		var writeOffset int64 = 0
-		for {
-			_, err := res.Body.Read(buff)
-			if err != nil {
-				logger.Printf("Error reading response: %s\n", err.Error())
-				return writeOffset
-			}
-			n, err := file.WriteAt(buff, writeOffset)
-			if err != nil {
-				logger.Printf("error writting to %s= %s\n", file.Name(), err.Error())
-				return writeOffset
-			}
-			writeOffset += int64(n)
-		}
-
-	}
-	n := bufferedWrite(res, file, logger)
-	fmt.Printf("# of bytes written %d -> %s\n", n, file.Name())
-}
-
 func parseHN(logger *log.Logger) {
 	hnURL := "https://news.ycombinator.com/news"
 	response, err := fetchHnPage(hnURL)
@@ -72,66 +38,9 @@ func parseHN(logger *log.Logger) {
 	b := response.Body
 	z := html.NewTokenizer(b)
 	hnscrapper.ParsePosts(z, logger)
-	// posts, err := parsePosts(z)
-	// if err != nil {
-	// 	log.Fatalln(err.Error())
-	// }
-
-	// for _, p := range posts {
-	// 	log.Println(p.String())
-	// }
-	// parsePosts(z)
 }
 
 func main() {
 	logger := log.New(os.Stdout, "HN-scrapper", log.Lshortfile)
 	parseHN(logger)
 }
-
-// switch currentState {
-// case stateInit:
-// 	// Looking for <tr id=$postId class="athing">
-// 	// on success: stateInit -> stateId
-// case stateID:
-// 	// Looking for storylink <a class = "storylink" href=$postURL
-// 	// on success: stateId -> stateStoryLink
-// case stateStoryLink:
-// 	// Looking for postTitle TextToken**
-// 	// on success: stateStoryLink ->stateStoryTitle
-// case stateStoryTitle:
-// 	// Looking for <span class="sitestr">
-// 	// on success: stateStoryTitle ->stateSiteStrIncoming
-// case stateSitestrIncoming:
-// 	// Looking for postSiteString TextToken**
-// 	// onSuccess: stateSiteStrIncoming -> stateSiteStr
-// case stateSiteStr:
-// 	// Looking for <span id ="score_$postId" class=score>
-// 	// onSuccess: stateSiteStr -> stateScoreIncoming
-// case stateScoreIncoming:
-// 	// Looking $score points TextToken**
-// 	// onSuccess: stateScoreIncoming -> stateScore
-// case stateScore:
-// 	// Looking for <a class="hnuser" href="user?id=$hnuser">
-// 	// onSuccess: stateScore -> stateHnuserIncoming
-// case stateHnuserIncoming:
-// 	// Looking for hnuser TextToken**
-// 	// onSuccess: stateHnuserIncoming -> stateHnuser
-// case stateHnuser:
-// 	// Looking for <span class="age">
-// 	// onSuccess: stateHnuser -> stateAgeIncoming1
-// case stateAgeIncoming1:
-// 	// Looking for <a id="item?id=$postID">
-// 	// onSuccess: stateAgeIncoming1 -> stateAgeIncoming2
-// case stateAgeIncoming2:
-// 	// Looking for $age hours ago TextToken**
-// 	// onSuccess: stateAgeIncoming2 -> stateAge
-// case stateAge:
-// 	// Looking for "hide" TextToken**
-// 	// stateAge -> stateNCommentsIncoming
-// case stateNCommentsIncoming:
-// 	// Looking for $nComments Comments TextToken**
-// 	// stateNCommentsIncoming -> stateNComments
-// case stateNComments:
-// 	// push *post into posts[]*Post
-// 	// stateNComments -> stateInit
-// }
